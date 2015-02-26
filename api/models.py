5f6a607a294ed1303c907c3b455428c7096ca397
eltronix <lt.eltronix@gmail.com>
2015-02-26 11:50:57 +0300

    added models for Intermediary, MeetingPoint and MeetingTime

--- api/models.py
+++ api/models.py
@@ -21,3 +21,25 @@ class Target(models.Model):
     places = models.ManyToManyField(Place, null=False)
 
 
+class Intermediary(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    other_name = models.CharField(max_length=50, null=True)
+    phone_no = models.CharField(max_length=20)
+    photo = models.TextField()
+
+
+class MeetingPoint(models.Model):
+    intermediary = models.ForeignKey(Intermediary, related_name='meeting_points')
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    gps_lat = models.CharField(max_length=50)
+    gps_long = models.CharField(max_length=50)
+    photo = models.TextField()
+
+
+class MeetingTime(models.Model):
+    meeting_point = models.ForeignKey(MeetingPoint, related_name='meeting_times')
+    day = models.CharField(max_length=20)
+    begin = models.CharField(max_length=10)
+    end = models.CharField(max_length=10)
