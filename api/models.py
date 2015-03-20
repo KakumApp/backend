c8c9e89ce6278e694fbe8418a994b7229e13f921
eltronix <lt.eltronix@gmail.com>
2015-03-20 12:23:03 +0300

    added blank model Target for other_name

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ class Place(models.Model):
 class Target(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    other_name = models.CharField(max_length=50, null=True)
+    other_name = models.CharField(max_length=50, null=True, blank=True)
     phone_no = models.CharField(max_length=20)
     photo = models.TextField()
     places = models.ManyToManyField(Place, null=False)
