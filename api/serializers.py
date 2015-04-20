658a1c2055cabb34e6881b3911b8da6a258a2596
eltronix <lt.eltronix@gmail.com>
2015-04-20 09:35:55 +0300

    updated search endpoint with expanded places

--- api/serializers.py
+++ api/serializers.py
@@ -2,15 +2,6 @@ from api.models import Country, Place, Target
 from rest_framework import serializers
 
 
-class TargetSerializer(serializers.ModelSerializer):
-    # places = serializers.ManyRelatedField(source='places.name', child_relation=Place)
-    # places = serializers.HyperlinkedRelatedField(view_name='place-detail', queryset=Place.objects.all(), many=True)
-
-    class Meta:
-        model = Target
-        fields = ('url', 'first_name', 'last_name', 'other_name', 'phone_no', 'photo', 'places')
-
-
 class PlaceSerializer(serializers.ModelSerializer):
     # def __init__(self, *args, **kwargs):
     #     many = kwargs.pop('many', True)
@@ -21,6 +12,16 @@ class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ('url', 'id', 'country', 'name')
+
+
+class TargetSerializer(serializers.ModelSerializer):
+    # places = serializers.ManyRelatedField(source='places.name', child_relation=Place)
+    # places = serializers.HyperlinkedRelatedField(view_name='place-detail', queryset=Place.objects.all(), many=True)
+    places = PlaceSerializer(many=True)
+
+    class Meta:
+        model = Target
+        fields = ('url', 'first_name', 'last_name', 'other_name', 'phone_no', 'photo', 'places')
 
 
 class CountrySerializerList(serializers.HyperlinkedModelSerializer):
